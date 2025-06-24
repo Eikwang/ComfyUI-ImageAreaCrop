6c7ffff5dbfd29adc2cdbd5792f04c734cd745b7
astink <75293747@qq.cpom>
2025-06-24 21:11:56 +0800

    修复 __init__.py 中 ImageReverseOrderNode 的导入问题

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
-from .nodes import ImageAreaCropNode, AreaCropRestoreNode
+from .nodes import ImageAreaCropNode, AreaCropRestoreNode, ImageReverseOrderNode
 
 NODE_CLASS_MAPPINGS = {
     "ImageAreaCropNode": ImageAreaCropNode,
-    "AreaCropRestoreNode": AreaCropRestoreNode
+    "AreaCropRestoreNode": AreaCropRestoreNode,
+    "ImageReverseOrderNode": ImageReverseOrderNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ImageAreaCropNode": "图像区域裁切",
-    "AreaCropRestoreNode": "区域裁切恢复"
+    "AreaCropRestoreNode": "区域裁切恢复",
+    "ImageReverseOrderNode": "图像倒序"
 }
